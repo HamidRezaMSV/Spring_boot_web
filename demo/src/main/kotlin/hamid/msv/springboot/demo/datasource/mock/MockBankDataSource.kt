928b9396606287f1be266310f2afe996fe6540b1
Hamid Reza Mousavi <hamidreza.m799@gmail.com>
2022-09-18 14:26:03 +0430

    create bank controller and use mockMvc for testing

--- demo/src/main/kotlin/hamid/msv/springboot/demo/datasource/mock/MockBankDataSource.kt
+++ demo/src/main/kotlin/hamid/msv/springboot/demo/datasource/mock/MockBankDataSource.kt
@@ -7,7 +7,11 @@ import org.springframework.stereotype.Repository
 @Repository
 class MockBankDataSource : BankDataSource{
 
-    private val banks = listOf(Bank("1234",1.0,1))
+    private val banks = listOf(
+        Bank("1234",1.0,1),
+        Bank("1235",2.0,2),
+        Bank("1236",3.0,3)
+    )
 
     override fun retrieveBanks(): Collection<Bank> = banks
 
